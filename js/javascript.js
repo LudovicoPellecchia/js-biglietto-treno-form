74604eaae8457e911c8176b4cb29c5a5deb2a7d8
LudovicoPellecchia <ludovico.pellecchia@gmail.com>
2023-07-04 10:40:57 +0200

    aggiunta reset button

--- js/javascript.js
+++ js/javascript.js
@@ -10,6 +10,7 @@ const minCarr = 1
 const maxCarr = 8
 const randomCarr = Math.floor(Math.random() * (maxCarr - minCarr + 1)) + minCarr;
 const randomCpCode = Math.floor(10000 + Math.random() * 90000)
+const ticketCol=document.getElementById("ticket-col")
 
 
 generateBtn.addEventListener("click", function() {
@@ -29,9 +30,11 @@ generateBtn.addEventListener("click", function() {
     else {
         ticketprice = ticketprice
     }
-
     
+
+
     if (userName && userAge && tripKm !="") {
+        ticketCol.classList.remove("d-none")
         document.getElementById("name-gen").innerHTML =`<strong>${userName}</strong>`
         document.getElementById("plan-gen").innerHTML =`Biglietto Standard`
         document.getElementById("carriage-gen").innerHTML =`${randomCarr}`
@@ -40,18 +43,14 @@ generateBtn.addEventListener("click", function() {
     }
 }) 
 
-
 cancelBtn.addEventListener("click", function(){
     tripKmInput.value=""
     userNameInput.value=""
     userAgeInput.value=""
-
+    ticketCol.classList.add("d-none")
     document.getElementById("name-gen").innerHTML =""
     document.getElementById("plan-gen").innerHTML =""
     document.getElementById("carriage-gen").innerHTML =""
     document.getElementById("cp-cod-gen").innerHTML =""
     document.getElementById("price-gen").innerHTML =""
 })
-
-
-
